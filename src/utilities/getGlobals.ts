40bc7d8824815f5960db27fca46a21c3d8c632df
InnerDesires <vlad.tertyshny@gmail.com>
2025-01-16 13:47:22 +0200

    refactor(getGlobals): update locale handling and cache revalidation
    - Changed the locale parameter type in getGlobal and getCachedGlobal functions from 'en' | 'uk' to TypedLocale for improved type safety.
    - Increased the cache revalidation time from 5 to 60 seconds to enhance performance and reduce unnecessary fetches.

--- src/utilities/getGlobals.ts
+++ src/utilities/getGlobals.ts
@@ -5,8 +5,9 @@ import { getPayload } from 'payload'
 import { unstable_cache } from 'next/cache'
 
 type Global = keyof Config['globals']
+import { TypedLocale } from 'payload'
 
-export async function getGlobal(slug: Global, locale: 'en' | 'uk', depth = 0) {
+export async function getGlobal(slug: Global, locale: TypedLocale, depth = 0) {
   const payload = await getPayload({ config: configPromise })
 
   const global = await payload.findGlobal({
@@ -21,8 +22,8 @@ export async function getGlobal(slug: Global, locale: 'en' | 'uk', depth = 0) {
 /**
  * Returns a unstable_cache function mapped with the cache tag for the slug
  */
-export const getCachedGlobal = (slug: Global, locale: 'en' | 'uk', depth = 0) =>
+export const getCachedGlobal = (slug: Global, locale: TypedLocale, depth = 0) =>
   unstable_cache(async () => getGlobal(slug, locale, depth), [slug], {
-    revalidate: 5,
+    revalidate: 60,
     tags: [`global_${slug}`],
   })
