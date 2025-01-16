d070e397506f86fa205c0f333ca200d78263ff1d
InnerDesires <vlad.tertyshny@gmail.com>
2025-01-16 18:09:36 +0200

    chore(config): update site URL and add console logs for debugging
    - Changed the default site URL in next-sitemap.config.cjs from 'https://example.com' to 'https://zzpayload.vercel.app'.
    - Added console log for NEXT_PUBLIC_SERVER_URL in next.config.js to aid in debugging.
    - Included console log for the image source URL in ImageMedia component to assist with image loading issues.

--- next.config.js
+++ next.config.js
@@ -8,6 +8,8 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
+
+console.log(NEXT_PUBLIC_SERVER_URL);
 const nextConfig = {
   images: {
     remotePatterns: [
