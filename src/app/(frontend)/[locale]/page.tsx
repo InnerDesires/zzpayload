33dca7ac56de68016b1dfbe513ff1bd2a9c953af
InnerDesires <vlad.tertyshny@gmail.com>
2025-01-17 18:46:11 +0200

    feat(locale): add static parameter generation for locales
    - Introduced `generateStaticParams` function to return an array of locale objects for 'uk' and 'en'.
    - This enhancement supports dynamic routing for localized pages, improving the overall structure of the locale handling in the application.

--- src/app/(frontend)/[locale]/page.tsx
+++ src/app/(frontend)/[locale]/page.tsx
@@ -1,6 +1,10 @@
 import PageTemplate, { generateMetadata } from './[slug]/page'
 
 
+export const generateStaticParams = async () => {
+  return ['uk', 'en'].map((locale) => ({ locale }))
+}
+
 export default PageTemplate
 
 export { generateMetadata }
