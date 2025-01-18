8c5a76bc6283f0e807874e081e4c53e133761e81
InnerDesires <vlad.tertyshny@gmail.com>
2025-01-18 10:49:58 +0200

    Revert "feat(locale): add static parameter generation for locales"
    This reverts commit 33dca7ac56de68016b1dfbe513ff1bd2a9c953af.

--- src/app/(frontend)/[locale]/page.tsx
+++ src/app/(frontend)/[locale]/page.tsx
@@ -1,10 +1,6 @@
 import PageTemplate, { generateMetadata } from './[slug]/page'
 
 
-export const generateStaticParams = async () => {
-  return ['uk', 'en'].map((locale) => ({ locale }))
-}
-
 export default PageTemplate
 
 export { generateMetadata }
